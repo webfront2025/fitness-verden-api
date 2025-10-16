8bccb231cd53d0c069808682d33df6cdc94cc665
wu11-mansoureh-safarian-toosi <U2442173@rts-365.dk>
2025-10-16 11:14:03 +0200

    16-10-1:13

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['localhost'], // Tillad billeder fra localhost
+      domains: ['localhost', 'trainerapi.onrender.com'],
     },
   };
  
